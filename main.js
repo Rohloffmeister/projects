98030c335539fd782558ab5a0ab89533f088b11b
Rohloffmeister <132856636+Rohloffmeister@users.noreply.github.com>
2024-11-05 22:42:17 +0100

    update

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
-console.log(document.body);
+const linksContainer = document.getElementById("links");
+let links = null;
+
+fetch("./links.json").then((response) => {
+    if (!response.ok) { throw new Error("Error loading json"); }
+    return response.json()
+}).then((data) => {
+    FillLinks(data);
+    return;
+})
+
+
+
+function FillLinks(data) {
+    console.log(data);
+    for(const element of data) {
+        console.log(element)
+        let container = `
+                  <div class="link-container">
+            <a
+              href="`+ element["links"]["livepage"] + `"
+              target="_blank"
+              ><button class="btn">`+ element["title"] +`</button></a
+            ><a
+              href="`+ element["links"]["githubrepo"] + `"
+              target="_blank"
+              ><button class="btn"><img src="assets/github.png" /></button>
+            </a>
+          </div>
+        `
+        linksContainer.innerHTML = linksContainer.innerHTML + container;
+    };
+    return 0;
+}
+
+
+
+
+
+
